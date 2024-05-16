f10da285d3b052d45a8b4e1c3c1dc0b1b7eed5d1
Phuocnq <pnhesi@gmail.com>
2024-05-16 15:22:15 +0700

    Feat Bound Player Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,36 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    Vector2 minBoundary;
+    Vector2 maxBoundary;
     Vector2 rawInput;
-
+    void Start()
+    {
+        InitBoundary();
+    }
     void Update()
     {
-        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Move();
     }
-
+    void InitBoundary()
+    {
+        Camera mainCamera = Camera.main;
+        minBoundary = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBoundary = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+    void Move()
+    {
+        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newpos = new Vector2();
+        newpos.x = Mathf.Clamp(transform.position.x + delta.x, minBoundary.x + paddingLeft, maxBoundary.x - paddingRight);
+        newpos.y = Mathf.Clamp(transform.position.y + delta.y, minBoundary.y + paddingBottom, maxBoundary.y - paddingTop);
+        transform.position = newpos;
+    }
     void OnMove(InputValue value)
     {
         rawInput = value.Get<Vector2>();
