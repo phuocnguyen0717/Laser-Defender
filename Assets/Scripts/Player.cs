e7ae148f18acc50593252ba772fae0ba48a686a7
Phuocnq <pnhesi@gmail.com>
2024-05-16 14:19:59 +0700

    Feat Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    Vector2 rawInput;
+
+    void Update()
+    {
+        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
+        transform.position += delta;
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+    }
+}
